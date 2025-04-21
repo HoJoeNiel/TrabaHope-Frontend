12d3056b6c353fa93dc9e33ed622e08c919c3af6
Nelman25 <jonelvillaver735@gmail.com>
2025-04-21 17:34:22 +0800

    install firebase, formik, and yup

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHVimg-_WTNtzbJ3pmAB7w-ZCBrAeUwjQ",
+  authDomain: "trabahope-19144.firebaseapp.com",
+  projectId: "trabahope-19144",
+  storageBucket: "trabahope-19144.firebasestorage.app",
+  messagingSenderId: "370944255383",
+  appId: "1:370944255383:web:41532cc32fe29f7670d0d2",
+  measurementId: "G-05LRTEQNP5",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
