a72a878b4ad6ec0f85effa294ae6f1ed2a2d26a7
Nelman25 <jonelvillaver735@gmail.com>
2025-04-25 05:55:48 +0800

    setup job store

---
+++ src/stores/useJobStore.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { Job } from "@/types";
+import { JOBS } from "@/constants/constants";
+
+type JobStore = {
+  jobs: Job[];
+  savedJobs: Job[];
+  appliedJobs: Job[];
+  addJobToSaved: (job: Job) => void;
+  removeJobToSaved: (job: Job) => void;
+};
+
+const jobStoreLogic = persist<JobStore>(
+  (set) => ({
+    jobs: JOBS,
+    savedJobs: [],
+    appliedJobs: [],
+
+    removeJobToSaved: (job) =>
+      set((state) => ({
+        savedJobs: state.savedJobs.filter((sj) => sj.id !== job.id),
+        jobs: state.jobs.map((j) =>
+          j.id !== job.id
+            ? j
+            : { ...job, actions: { ...job.actions, saved: !job.actions.saved } }
+        ),
+      })),
+
+    addJobToSaved: (job) =>
+      set((state) => ({
+        savedJobs: [...state.savedJobs, job],
+        jobs: state.jobs.map((j) =>
+          j.id !== job.id
+            ? j
+            : { ...job, actions: { ...job.actions, saved: !job.actions.saved } }
+        ),
+      })),
+  }),
+  { name: "job-storage" }
+);
+
+export const useJobStore = create<JobStore>()(jobStoreLogic);
