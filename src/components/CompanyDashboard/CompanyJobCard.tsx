c7e4e603978fc1202337f8b379b2af36c3e7f12b
Jonel Villaver <jonelvillaver735@gmail.com>
2025-05-27 09:21:12 +0800

    add menu field

--- src/components/CompanyDashboard/CompanyJobCard.tsx
+++ src/components/CompanyDashboard/CompanyJobCard.tsx
@@ -2,20 +2,27 @@ import { formatDate, getRelativeTimeAgo } from "@/helpers";
 import { CompanyPostedJob } from "@/types";
 import {
   Clock,
-  EllipsisVertical,
+  Edit,
   Eye,
   MapPin,
+  MoreVertical,
   Share,
   Users,
 } from "lucide-react";
+import { useState } from "react";
+import { Badge } from "../ui/badge";
+import { useNavigate } from "react-router-dom";
+import DeleteJobDialog from "../DeleteJobDialog";
 
 type CompanyJobsCardProps = {
   job: CompanyPostedJob;
 };
 
 export default function CompanyJobCard({ job }: CompanyJobsCardProps) {
+  const navigate = useNavigate();
   const {
     companyName,
+    id,
     companyProfileUrl,
     description,
     employmentType,
@@ -26,6 +33,7 @@ export default function CompanyJobCard({ job }: CompanyJobsCardProps) {
     tags,
     timestamps,
   } = job;
+  const [showMenu, setShowMenu] = useState(false);
 
   const avatar = companyProfileUrl ?? companyName.charAt(0).toUpperCase();
 
@@ -44,13 +52,39 @@ export default function CompanyJobCard({ job }: CompanyJobsCardProps) {
           </div>
         </div>
 
-        <div className="flex items-center space-x-2">
-          <span className="block px-2 font-medium text-green-700 bg-green-200 rounded-full">
+        <div className="flex items-center gap-2">
+          <Badge className="text-sm bg-green-400 hover:bg-green-400">
             Active
-          </span>
-          <button onClick={() => console.log("menu rito")}>
-            <EllipsisVertical size={18} />
-          </button>
+          </Badge>
+          <div className="relative">
+            <button
+              onClick={() => setShowMenu(!showMenu)}
+              className="p-1 rounded-full hover:bg-gray-100"
+            >
+              <MoreVertical size={18} className="text-gray-500" />
+            </button>
+
+            {showMenu && (
+              // TODOs: Function para sa mga buttons na to
+              <div className="absolute right-0 z-50 w-48 mt-1 bg-white border border-gray-100 rounded-md shadow-lg animate-fadeIn">
+                <div className="py-1">
+                  <button
+                    onClick={() => navigate(`/recruiter/job/${job.id}`)}
+                    className="flex items-center w-full gap-2 px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100"
+                  >
+                    <Eye size={16} /> View Job
+                  </button>
+                  <button
+                    onClick={() => navigate(`/recruiter/edit-job/${id}`)}
+                    className="flex items-center w-full gap-2 px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100"
+                  >
+                    <Edit size={16} /> Edit Job
+                  </button>
+                  <DeleteJobDialog id={id} />
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
@@ -59,9 +93,9 @@ export default function CompanyJobCard({ job }: CompanyJobsCardProps) {
         <MapPin />
         <span>{location}</span>
         {remote && (
-          <span className="block px-2 font-medium text-blue-700 bg-blue-200 rounded-full">
+          <Badge className="text-sm text-blue-700 bg-blue-200 hover:bg-blue-200">
             Remote
-          </span>
+          </Badge>
         )}
         <Clock />
         <span>Posted {getRelativeTimeAgo(timestamps.posted)}</span>
@@ -76,11 +110,11 @@ export default function CompanyJobCard({ job }: CompanyJobsCardProps) {
       <div className="flex justify-between px-4 my-3">
         <div>
           <span className="block text-xs text-gray-600">Salary Range</span>
-          <span className="block font-medium">{salaryRange}</span>
+          <span className="block font-medium">{`₱${salaryRange.min} - ₱${salaryRange.min}`}</span>
         </div>
 
         <div>
-          <span className="block text-xs text-gray-600">Employment Type</span>
+          <span className="block text-xs text-gray-600"> Employment Type</span>
           <span className="block font-medium">{employmentType}</span>
         </div>
 
@@ -99,11 +133,11 @@ export default function CompanyJobCard({ job }: CompanyJobsCardProps) {
         </p>
       </div>
 
-      <div className="flex px-4 my-3 space-x-2">
+      <div className="flex items-center justify-center mx-auto px-4 my-3 space-x-2 overflow-x-scroll max-w-[95%] no-scrollbar">
         {tags.map((tag) => (
           <span
             key={tag}
-            className="block px-3 text-sm text-gray-700 bg-gray-100 rounded-sm hover:bg-gray-200"
+            className="block min-w-[100px] px-4 text-sm text-center text-gray-700 bg-gray-100 rounded-sm hover:bg-gray-200"
           >
             {tag}
           </span>
