b648ddb586b745d344a73e9597d093e03108b45e
Jonel Villaver <jonelvillaver735@gmail.com>
2025-06-12 22:32:07 +0800

    Introduced a fallback hero and message when no jobs have been saved yet

--- src/pages/Applicant/SavedJobsPage.tsx
+++ src/pages/Applicant/SavedJobsPage.tsx
@@ -1,4 +1,6 @@
+import noSavedJobs from "@/assets/Empty-bro.svg";
 import JobCard from "@/components/JobListingPage/JobCard";
+import Loading from "@/components/Loading";
 import { isApplicant } from "@/helpers";
 import { useSavedJobs } from "@/services/queries";
 import { useLoggedInUserStore } from "@/stores/useLoggedInUserStore";
@@ -9,9 +11,7 @@ export default function SavedJobsPage() {
   if (!isApplicant(applicant))
     throw new Error("User logged in is not an applicant.");
 
-  const { data: savedJobs } = useSavedJobs(applicant.id);
-
-  console.log(savedJobs);
+  const { data: savedJobs, isPending } = useSavedJobs(applicant.id);
 
   return (
     <div className="flex flex-col min-h-screen p-4 min-w-screen">
@@ -23,6 +23,16 @@ export default function SavedJobsPage() {
           </p>
         </div>
 
+        {isPending && <Loading />}
+        {savedJobs?.length === 0 && !isPending && (
+          <div className="flex flex-col w-full justify-center items-center h-[700px]">
+            <img src={noSavedJobs} alt="No saved jobs" className="w-[400px]" />
+            <p className="text-lg text-gray-300">
+              You haven’t saved any job postings yet. Start saving jobs to
+              easily find them later!
+            </p>
+          </div>
+        )}
         {savedJobs?.map((job) => (
           <JobCard key={job.id} job={job} />
         ))}
