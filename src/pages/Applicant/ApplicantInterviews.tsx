3285f7a1dd027222cc6a0e55e1d209ab8385fe42
Jonel Villaver <jonelvillaver735@gmail.com>
2025-06-12 22:34:36 +0800

    feat: complete applicant interviews page
    - Improved InterviewDetailsCard to support editing interview details
    - Made the component flexible for use on both applicant and recruiter views

--- src/pages/Applicant/ApplicantInterviews.tsx
+++ src/pages/Applicant/ApplicantInterviews.tsx
@@ -1,23 +1,31 @@
 import InterviewDetailsCard from "@/components/InterviewsPage/InterviewDetailsCard";
+import Loading from "@/components/Loading";
 import { isApplicant } from "@/helpers";
 import { useApplicantInterviews } from "@/services/queries";
 import { useLoggedInUserStore } from "@/stores/useLoggedInUserStore";
+import noInterviews from "@/assets/Schedule-bro (1).svg";
 
 export default function ApplicantInterviews() {
   const applicant = useLoggedInUserStore((state) => state.user);
 
   if (!isApplicant(applicant)) throw new Error("User is not an applicant.");
 
-  const {
-    data: interviews,
-    isPending,
-    isError,
-  } = useApplicantInterviews(applicant.id);
+  const { data: interviews, isPending } = useApplicantInterviews(applicant.id);
 
   return (
     <div className="flex-1 p-4">
-      <h1 className="text-2xl font-semibold">Interviews</h1>
+      <h1 className="text-2xl font-semibold text-white">Interviews</h1>
+      {!isPending && interviews?.length === 0 && (
+        <div className="flex flex-col w-full justify-center items-center h-[700px]">
+          <img src={noInterviews} alt="No jobs found" className="w-[400px]" />
+          <p className="text-lg text-gray-300">
+            No jobs match your current filters, resume, or interests. Try
+            adjusting your search to see more opportunities.
+          </p>
+        </div>
+      )}
       <div className="grid grid-cols-2 gap-12 py-8 max-2xl:grid-cols-1">
+        {isPending && <Loading />}
         {interviews &&
           interviews.map((interview) => (
             <InterviewDetailsCard
